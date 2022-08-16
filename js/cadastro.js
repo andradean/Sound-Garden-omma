179ddcecad8fef38ffd97816f1e4a6c39ae2886b
Vinicius A N Silva <viniciusansilva@outlook.com>
2022-08-16 14:42:25 -0300

    reformatando pastas e corrigindo erro adminjs

---
+++ js/cadastro.js
@@ -0,0 +1,48 @@
+const API_URL = "https://xp41-soundgarden-api.herokuapp.com";
+const inputNome = document.querySelector("#nome");
+const evento = document.querySelector("#eventos");
+const inputBanner = document.querySelector("#banner");
+const inputAtracoes = document.querySelector("#atracoes");
+const inputDescricao = document.querySelector("#descricao");
+const inputData = document.querySelector("#data");
+const inputLotacao = document.querySelector("#lotacao");
+const form = document.querySelector("form");
+
+form.onsubmit = async (evento) => {
+  evento.preventDefault();
+
+  const novoEvento = {
+    name: inputNome.value,
+    poster: inputBanner.value,
+    attractions: inputAtracoes.value.split(","),
+    description: inputDescricao.value,
+    scheduled: inputData.value.slice(0, 16),
+    number_tickets: inputLotacao.value,
+  };
+
+  const options = {
+    method: "POST",
+    body: JSON.stringify(novoEvento),
+    headers: { "Content-Type": "application/json" },
+    redirect: "follow",
+  };
+
+  const resposta = await fetch(`${API_URL}/events`, options);
+  const conteudoResposta = await resposta.json();
+  console.log(conteudoResposta);
+
+  if (resposta.status == 201) {
+    alert("Evento cadastrado com sucesso!");
+    window.location.href = "admin.html";
+
+    inputNome.value = "";
+    inputBanner.value = "";
+    inputAtracoes.value = "";
+    inputDescricao.value = "";
+    inputData.value = "";
+    inputLotacao.value = "";
+  }
+  if (resposta.status != 201) {
+    alert("algo deu errado!!!");
+  }
+};
