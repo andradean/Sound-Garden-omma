24bdc95e710d73d02b68c47c979fd0b5b9b64bda
LuanaGSouza21 <63968764+lusouzarego@users.noreply.github.com>
2022-08-17 12:34:42 +0200

    primeiro banner

--- soundGarden/js/index.js
+++ soundGarden/js/index.js
@@ -11,3 +11,21 @@ function fecharModal(){
     
 }
 
+
+const imagens = document.getElementById("imagens");
+const imagem = document.querySelectorAll("#imagens imagens");
+
+
+let idX = 0;
+
+function carrossel(){
+    idX++;
+
+    if(idX > imagem.lenght - 1){
+        idX = 0;
+    }
+
+    imagens.style.transform = `translateX(${-idx * 500}px)`;
+}
+
+setInterval(carrossel, 2000);
---
+++ soundGarden/js/reserva.js
@@ -0,0 +1,2 @@
+const BASE_URL = "https://xp41-soundgarden-api.herokuapp.com";
+
